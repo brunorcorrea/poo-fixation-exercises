15e8f7d61e3b02fd2d7e08851d7ad1e2414077ff
Bruno Ricardo Corrêa <60991787+brunorcorrea@users.noreply.github.com>
2023-04-06 21:58:12 -0300

    first commit

--- block-1/ex4/cmake-build-debug/cmake_install.cmake
+++ block-1/ex4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/corre/CLionProjects/poo/ex4
+# Install script for directory: C:/Users/corre/CLionProjects/poo/block-1/ex4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/corre/CLionProjects/poo/ex4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/corre/CLionProjects/poo/block-1/ex4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
